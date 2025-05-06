4e6fabceefde5fc65e11fb63ae6e80f7f6f72933
brk275 <88346297+brk275@users.noreply.github.com>
2025-05-06 19:41:06 -0400

    Delete sample.py

--- sample.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-
-df_sample = pd.read_csv(r"C:\Users\Alex\Downloads\DOB_Permit_Issuance_20250504.csv", nrows=1000)
-
-# Save it to a new file
-df_sample.to_csv("sample_permit_data.csv", index=False)
